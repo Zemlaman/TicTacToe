2c1f8bf90246be97761213cbed0db74b99c69db9
Zemlaman <matej.zemla@seznam.cz>
2021-06-06 19:30:45 +0200

    🙃2.0👻🦘👥🗣

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -85,11 +85,23 @@ public class Game {
 
     //[1][1](střed)
     public static char hasWon(char[][] board) {
-        for (int x = 0; x < 3; x++) {
-            if (board[x][0] == board[x][2] && board[1][x] == board[2][x] && board[0][x] != '-') {
-                return board[0][x];
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != '-') {
+                return board[i][0];
             }
-            //dodelat vyhru v radku a sikmo
+        }
+        //sloupec
+        for (int j = 0; j < 3; j++) {
+            if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[0][j] != '-') {
+                return board[0][j];
+            }
+        }
+        //radek
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != '-') {
+            return board[0][0];
+        }
+        if (board[2][0] == board[1][1] && board[1][1] == board[0][2] && board[2][0] != '-') {
+            return board[2][0];
         }
         return ' ';
     }
